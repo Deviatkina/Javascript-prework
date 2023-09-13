19dfb2468e9b078e1567d8ec1dc8ab150d6082fa
Anastasiia Deviatkina <anastasia.deviatkina@gmail.com>
2023-09-13 09:01:12 +0200

    Replaced all in file js/Function.js by the code for computerMove maening

--- js/function.js
+++ js/function.js
@@ -1,14 +1,16 @@
-function printMessage(msg) {
-    let div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-}
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-}
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = `papier`;
-printMessage('Zagrałem ' + playerMove + '!');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+    playerMove = 'papier';
+  }
+else(playerInput == '3'){
+    playerMove = 'nożyce';
+  }
+printMessage('Twój ruch to: ' + playerMove);
